8113bcc3adfd449a86bcd2a806dfeb0af8c0510b
Cedric <75742716+cedorikku@users.noreply.github.com>
2023-12-08 02:08:04 +0800

    Updates and bug fixes

--- ShoTacToe/app/build.gradle.kts
+++ ShoTacToe/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.shotactoe"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.shotactoe"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
