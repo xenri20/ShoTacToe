d07ddea424349469bd836cf3af1f4c5a670ea047
Cedric <75742716+cedorikku@users.noreply.github.com>
2023-12-08 19:21:46 +0800

    Remove unused import

--- ShoTacToe/app/src/main/java/com/example/shotactoe/ResultDialog.java
+++ ShoTacToe/app/src/main/java/com/example/shotactoe/ResultDialog.java
@@ -1,7 +1,6 @@
 package com.example.shotactoe;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Context;
