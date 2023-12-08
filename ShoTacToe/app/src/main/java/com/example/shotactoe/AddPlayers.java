103abe15af999a517072cf3e795609f83c7fce96
Cedric <75742716+cedorikku@users.noreply.github.com>
2023-12-08 18:06:36 +0800

    Add background music on the main menu

--- ShoTacToe/app/src/main/java/com/example/shotactoe/AddPlayers.java
+++ ShoTacToe/app/src/main/java/com/example/shotactoe/AddPlayers.java
@@ -3,6 +3,7 @@ package com.example.shotactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,9 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class AddPlayers extends AppCompatActivity {
+
+    // Initialize media player
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,36 @@ public class AddPlayers extends AppCompatActivity {
                 }
             }
         });
+    }
 
+    public void playBackgroundMusic(View v) {
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.background_music);
+            // loop the song
+            mediaPlayer.setLooping(true);
+        }
+        mediaPlayer.start();
+    }
+
+    public void stopBackgroundMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    // When the activity is resumed, if the background music is not playing, start playing the background music
+    @Override
+    protected void onResume() {
+        super.onResume();
+        playBackgroundMusic(null);
+    }
+
+    // When the activity is paused, stop playing the background music
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopBackgroundMusic();
     }
 }
